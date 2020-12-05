a9c8bf69037cb6eb35427e9c13b98378ef94777f
Herman Lundkvist <herman.lundkvist@gmail.com>
2020-12-05 12:45:20 +0100

    Day4 part 2

--- 4/1.cpp
+++ 4/1.cpp
@@ -10,6 +10,7 @@
 #include <set>
 #include <map>
 #include <functional>
+#include <regex>
 
 using KeySet = std::set<std::string>;
 using KeyValidators = std::map<std::string, std::function<bool(std::string const&)>>;
@@ -33,20 +34,51 @@ void UpdateKeys(KeyValidators const &validators, std::set<std::string> &remainin
     std::cout << line << std::endl;
 }
 
+template<int lower, int higher>
+bool ValidInterval(std::string const& val) {
+    int i = std::stoi(val);
+    return (val.size() == 4) && (i >= lower) && (i <= higher);
+}
+
+int Height(std::string const& val) {
+    auto re = std::regex("([0-9]+)(in|cm)");
+    std::smatch match{}; 
+    if (std::regex_match(val, match, re)) {
+        int i = std::stoi(match[1]);
+        if (match[2] == "cm") {
+            return (150 <= i) && (193 >= i);
+        } else {
+            return (59 <= i) && (76 >= i);
+        }
+    } else {
+        return false;
+    }
+}
+
 int main(int argc, char* argv[]) {
     std::uint32_t valid = 0;
     std::string line{};
     KeySet all_keys{"byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"};
     KeyValidators validators{
-        {"byr", [](std::string const& val)-> bool {
-                      int i = std::stoi(val);
-                      return (i >= 1920) && (i <= 2020);}},
-        {"iyr", [](std::string const& val)-> bool {return true;}},
-        {"eyr", [](std::string const& val)-> bool {return true;}},
-        {"hgt", [](std::string const& val)-> bool {return true;}},
-        {"hcl", [](std::string const& val)-> bool {return true;}},
-        {"ecl", [](std::string const& val)-> bool {return true;}},
-        {"pid", [](std::string const& val)-> bool {return true;}},
+        {"byr", ValidInterval<1920,2002>},
+        {"iyr", ValidInterval<2010,2020>},
+        {"eyr", ValidInterval<2020,2030>},
+        {"hgt", Height},
+        {"hcl", [](std::string const& val)-> bool {
+                auto re = std::regex("#[0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f]");
+                return std::regex_match(val, re);
+            }
+        },
+        {"ecl", [](std::string const& val)-> bool {
+                auto re = std::regex("(amb|blu|brn|gry|grn|hzl|oth)");
+                return std::regex_match(val, re);
+            }
+        },
+        {"pid", [](std::string const& val)-> bool {
+                auto re = std::regex("[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]");
+                return std::regex_match(val, re);
+            }
+        },
         {"cid", [](std::string const&)-> bool {return true;}}
     };
 
