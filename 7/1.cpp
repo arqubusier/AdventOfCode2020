e07edbcd35aecc3c57a86730e098bc791feab284
Herman Lundkvist <herman.lundkvist@gmail.com>
2020-12-07 21:54:19 +0100

    Day 7 part1

--- 7/1.cpp
+++ 7/1.cpp
@@ -7,81 +7,61 @@
 #include <sstream>
 #include <vector>
 #include <array>
+#include <list>
 #include <set>
 #include <map>
 #include <functional>
 #include <regex>
 
 
-/*
- light gold mirrored brown
-26	        std::string remainder{match_min.suffix()};
-(gdb) 
-27	        while (remainder != ".") {
-(gdb) 
-28	            std::cout << " "  <<  remainder << std::endl;
-(gdb) 
- , 1 muted chartreuse bag, 2 wavy yellow bags.
-29	            std::smatch match_rest{}; 
-(gdb) 
-30	            std::regex_search(remainder, match_rest, re_rest);
-(gdb) 
-31	            std::cout << "-" << match_rest[1] << std::endl;
-(gdb) 
--muted chartreuse
+using ChildrenT = std::list<std::string>;
+using RulesT = std::map<std::string, ChildrenT>;
 
-Breakpoint 1, main () at 1.cpp:32
-32	            remainder = match_rest.suffix();
-(gdb) 
-29	            std::smatch match_rest{}; 
-(gdb) 
-27	        while (remainder != ".") {
-(gdb) 
-28	            std::cout << " "  <<  remainder << std::endl;
-(gdb) 
- , 2 wavy yellow bags.
-29	            std::smatch match_rest{}; 
-(gdb) 
-30	            std::regex_search(remainder, match_rest, re_rest);
-(gdb) 
-31	            std::cout << "-" << match_rest[1] << std::endl;
-(gdb) 
--wavy yellow
+void EventuallyContains(RulesT const& rules, std::string const &containee, std::set<std::string> &containers) {
+    auto rule{rules.find(containee)};
 
-Breakpoint 1, main () at 1.cpp:32
-32	            remainder = match_rest.suffix();
-(gdb) 
-29	            std::smatch match_rest{}; 
-(gdb) 
-27	        while (remainder != ".") {
-(gdb) 
-26	        std::string remainder{match_min.suffix()};
-(gdb) 
-22	        std::smatch match_min{}; 
-(gdb) 
-20	        auto re_rest = std::regex(", [0-9]+ (.*?) bags?");
-(gdb) 
-*/
+    if (rule != rules.end()) {
+        for (auto container: rule->second) {
+            containers.insert(container);
+            EventuallyContains(rules, container, containers);
+        }
+    }
+}
 
 int main() {
 
     std::string line{};
+    RulesT rules{};
     while (std::getline(std::cin, line)){
         auto re_min = std::regex("(.*?) bags? contain [0-9]+ (.*?) bags?");
         auto re_rest = std::regex(", [0-9]+ (.*?) bags?");
+        auto re_skip = std::regex("(.*?) bags? contain no other bags.");
+
+        if (std::regex_match(line, re_skip)) {
+            std::cout << " skip " << std::endl;
+            continue;
+        }
 
         std::smatch match_min{}; 
         std::regex_search(line, match_min, re_min);
-        std::cout << match_min[1] << " " << match_min[2] << std::endl;
+        std::string container{match_min[1]};
+        std::string containee{match_min[2]};
+        rules[containee].push_back(container);
+        std::cout << container << "," << containee << std::endl;
 
         std::string remainder{match_min.suffix()};
         while (remainder != ".") {
-            std::cout << " "  <<  remainder << std::endl;
             std::smatch match_rest{}; 
             std::regex_search(remainder, match_rest, re_rest);
-            std::cout << "-" << match_rest[1] << std::endl;
+            std::string containee{match_rest[1]};
+            rules[containee].push_back(container);
             remainder = match_rest.suffix();
+            std::cout << "-" << containee << std::endl;
         }
     }
+    
+    std::set<std::string> containers;
+    EventuallyContains(rules, "shiny gold", containers);
+    std::cout << containers.size() << std::endl;
     return 0;
 }
